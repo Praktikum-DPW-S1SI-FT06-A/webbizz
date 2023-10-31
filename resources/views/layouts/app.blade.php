deb314cb5657a7773648a315fd7cf293bb387c84
weedstream <dede.ipang426@gmail.com>
2023-11-01 06:28:46 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,7 @@
                             <script src="{{ asset('js/scripts.js') }}"></script>
                         </li>
                     </ul>
-                    <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2022</div>
+                    <div class="small text-center text-muted fst-italic">Copyright &copy; Praktikum DPW A</div>
                 </div>
             </div>
         </div>
